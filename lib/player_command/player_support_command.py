b0164731b3eab8b825e7f91ec7e20c1bedbed55d
Aref <arefsayareh@gmail.com>
2019-07-12 11:38:39 -0400

    add some commands

--- lib/player_command/player_support_command.py
+++ lib/player_command/player_support_command.py
@@ -26,14 +26,54 @@ class PlayerTurnNeckCommand(PlayerSupportCommand):
         return self._moment
 
 
+# TODO kirie in :\
 # class PlayerChangeViewCommand(PlayerSupportCommand):
 #     def __init__(self):
 
 
 class PlayerSayCommand(PlayerSupportCommand):
     def __init__(self, msg: str, version: float):
-        self_msg = msg
+        self._msg = msg
         self._version = version
 
     def str(self):
-        return f"(say {self._msg}"
+        tmp = self._msg
+        if self._version >= 8:
+            tmp = '"' + self._msg + '"'
+        return f"(say {tmp})"
+
+    def type(self):
+        return CommandType.SAY
+
+    def message(self):
+        return self._msg
+
+    def append(self, msg):
+        self._msg += msg
+
+
+class PlayerPointtoCommand(PlayerSupportCommand):
+    def __init__(self, dist: float, dir: float, on: bool):
+        self._dist = dist
+        self._dir = dir  # relative to body angle
+        self._on = on  # on/off switch
+
+    def str(self):
+        if self._on:
+            return f"(pointto {self._dist} {self._dir})"
+        else:
+            return "(pointto off)"
+
+    def type(self):
+        return CommandType.POINTTO
+#TODO Write other commands ...
+
+class PlayerDoneCommand(PlayerSupportCommand):
+    def __init__(self):
+        pass
+
+    def str(self):
+        return "(done)"
+
+    def type(self):
+        return CommandType.DONE
