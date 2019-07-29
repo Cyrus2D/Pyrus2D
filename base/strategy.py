1f995baf144074ad0148deea4333fa01330a01b7
Nader Zare <nader.zare88@gmail.com>
2019-07-29 06:18:18 +0430

    add new strategy for formation

--- base/strategy.py
+++ base/strategy.py
@@ -1,7 +1,7 @@
 from lib.math.geom_2d import *
 
 
-class Strategy:
+class _Strategy:
     def __init__(self):
         self._base_poses = []
         self._base_poses.append([Vector2D(-45, 0), 7, 10])
@@ -17,7 +17,8 @@ class Strategy:
         self._base_poses.append([Vector2D(30, 0), 15, 15])
         self._poses = [Vector2D(0, 0) for i in range(11)]
 
-    def update(self, ball_pos):
+    def update(self, wm):
+        ball_pos = wm.ball().pos()
         for p in range(len(self._poses)):
             x = ball_pos.x() / 52.5 * self._base_poses[p - 1][1] + self._base_poses[p - 1][0].x()
             y = ball_pos.y() / 52.5 * self._base_poses[p - 1][1] + self._base_poses[p - 1][0].y()
@@ -25,3 +26,11 @@ class Strategy:
 
     def get_pos(self, unum):
         return self._poses[unum - 1]
+
+
+class Strategy:
+    _i: _Strategy = _Strategy()
+
+    @staticmethod
+    def i() -> _Strategy:
+        return Strategy._i
