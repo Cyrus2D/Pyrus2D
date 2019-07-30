1b435bc5dfbba79108e889533e4ece376a71a84e
Nader Zare <nader.zare88@gmail.com>
2019-07-30 04:43:20 +0430

    add penalty

--- lib/rcsc/game_mode.py
+++ lib/rcsc/game_mode.py
@@ -1,16 +1,16 @@
-from lib.rcsc.types import SideID, PlayMode
+from lib.rcsc.types import SideID, GameModeType
 
 
 class GameMode:
-    def __init__(self, game_mode: PlayMode = None):
-        self._game_mode: PlayMode = game_mode
+    def __init__(self, game_mode: GameModeType = None):
+        self._game_mode: GameModeType = game_mode
         self._mode_name: str = None
         self._side: SideID = None
         if game_mode is not None:
             self._mode_name = self._set_mode_name()
             self._side = self._set_side()
 
-    def mode(self) -> PlayMode:
+    def type(self) -> GameModeType:
         return self._game_mode
 
     def side(self) -> SideID:
@@ -29,7 +29,7 @@ class GameMode:
             return self._game_mode.value[:-2]
         return self._game_mode.value
 
-    def set_game_mode(self, play_mode: PlayMode):
+    def set_game_mode(self, play_mode: GameModeType):
         self.__init__(play_mode)
 
     def is_teams_set_play(self, team_side: SideID):
@@ -50,6 +50,12 @@ class GameMode:
                 mode_name == "catch_fault":
             return self.side() != team_side
         return False
+
+    def is_penalty_kick_mode(self):
+        if self.type() in [GameModeType.PenaltySetup_Left, GameModeType.PenaltySetup_Right, GameModeType.PenaltyReady_Left, GameModeType.PenaltyReady_Right, GameModeType.PenaltyTaken_Left, GameModeType.PenaltyReady_Right, GameModeType.PenaltyMiss_Left, GameModeType.PenaltyMiss_Right, GameModeType.PenaltyScore_Left, GameModeType.PenaltyScore_Right]:
+            return True
+        return False
+        # todo add PlayMode.PenaltyOnfield, PenaltyFoul_
 
     def is_our_set_play(self, our_side: SideID):
         return self.is_teams_set_play(our_side)
