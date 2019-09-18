0d30325af900db1aa7c9ee6673493f02d28b140e
Aref <arefsayareh@gmail.com>
2019-09-18 14:26:24 -0400

    add basic client

---
+++ lib/player/basic_client.py
@@ -0,0 +1,23 @@
+class BasicClient:
+    def __init__(self):
+        pass
+
+    def connect_to(self,
+                   host_port: tuple,
+                   interval_ms):
+        pass
+
+    def run(self, agent):
+        pass
+
+    def run_online(self, agent):
+        pass
+
+    def send_message(self, msg):
+        pass
+
+    def recv_message(self):
+        pass
+
+    def message(self):
+        pass
