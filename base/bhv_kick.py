3e7089678838a6a209d3461e0600c990c9a2d687
Nader Zare <nader.zare88@gmail.com>
2019-11-16 16:34:52 -0400

    bhvkick: fix bug in evaluate candidate

--- base/bhv_kick.py
+++ base/bhv_kick.py
@@ -4,6 +4,7 @@ from lib.math.geom_2d import *
 from enum import Enum
 from lib.rcsc.server_param import ServerParam as SP
 from lib.action.smart_kick import SmartKick
+from typing import List
 
 
 class KickActionType(Enum):
@@ -23,7 +24,13 @@ class KickAction:
         self.eval = 0
 
     def __gt__(self, other):
-        return self.eval > other.eval
+        return self.eval < other.eval
+
+    def __repr__(self):
+        return 'KAction {} to {} in {} eval:{}'.format(self.start_unum, self.target_unum, self.target_ball_pos, self.eval)
+
+    def __str__(self):
+        return self.__repr__()
 
 
 class BhvPassGen:
@@ -100,23 +107,21 @@ class BhvKick:
         pass
 
     def evaluator(self, candid: KickAction):
-        return candid.target_ball_pos.dist(Vector2D(52, 0))
+        return candid.target_ball_pos.x() + max(0, 40 - candid.target_ball_pos.dist(Vector2D(52, 0)))
 
     def execute(self, agent):
         wm: WorldModel = agent.world()
-        action_candidates = []
+        action_candidates: List[KickAction] = []
         BhvPassGen().generator(wm, action_candidates)
         if len(action_candidates) is 0:
             return True
         for action_candidate in action_candidates:
-            self.evaluator(action_candidate)
+            action_candidate.eval = self.evaluator(action_candidate)
 
         best_action: KickAction = max(action_candidates)
 
         target = best_action.target_ball_pos
-        # angle: AngleDeg = (target - wm.self().pos()).th()
-        # angle -= wm.self().body()
-        print("Target :", target , " Speed : " , best_action.start_ball_speed)
+        print("target len:", len(action_candidates), "Target :", target , " Speed : " , best_action.start_ball_speed)
         SmartKick(target, best_action.start_ball_speed, best_action.start_ball_speed - 1, 3).execute(agent)  # best_action.start_ball_speed, 0.7, 3).execute(agent)
 
         # agent.do_kick(100, angle)
