fa5dc67fba9af7efc4fb6d3431dfeacbc141bcc9
Nader Zare <nader.zare88@gmail.com>
2019-11-17 14:59:39 -0400

    add direct pass like agent 2d

--- base/bhv_kick.py
+++ base/bhv_kick.py
@@ -51,47 +51,200 @@ class BhvKickGen:
 
 class BhvPassGen(BhvKickGen):
     def generator(self, wm: WorldModel):
-        for tm_unum in range(1, 12):
-            self.generate_direct_pass(wm, tm_unum)
+        self.receivers = []
+        self.update_receivers(wm)
+        print('##', wm.time().cycle())
+        for r in self.receivers:
+            self.generate_direct_pass(wm, r)
+            self.generate_lead_pass(wm, r)
+            self.generate_through_pass(wm, r)
         return self.candidates
 
-    def generate_direct_pass(self, wm: WorldModel, unum):
-        simple_direct_pass = KickAction()
-        if wm.our_player(unum).unum() is 0:
-            return
-        tm = wm.our_player(unum)
-        if tm.unum() == wm.self().unum():
-            return
-        intercept_cycle = 0
-        if wm.self().is_kickable():
-            intercept_cycle = 0
-        simple_direct_pass.type = KickActionType.Pass
-        simple_direct_pass.start_ball_pos = wm.ball().pos()
-        simple_direct_pass.target_ball_pos = tm.pos()
-        simple_direct_pass.target_unum = unum
-        simple_direct_pass.start_ball_speed = 2.0
+    def update_receivers(self, wm: WorldModel):
+        sp = ServerParam.i()
+        for unum in range(12):
+            if unum == wm.self().unum():
+                continue
+            if wm.our_player(unum).unum() == 0:
+                continue
+            if wm.our_player(unum).pos_count() > 10:
+                continue
+            if wm.our_player(unum).is_tackling():
+                continue
+            if wm.our_player(unum).pos().x() > wm.offside_line_x():
+                continue
+            if wm.our_player(unum).goalie() and wm.our_player(unum).pos().x() < sp.our_penalty_area_line_x() + 15:
+                continue
+            self.receivers.append(unum)
 
-        pass_dist = simple_direct_pass.start_ball_pos.dist(simple_direct_pass.target_ball_pos)
-        ball_speed = simple_direct_pass.start_ball_speed
-        ball_vel: Vector2D = Vector2D.polar2vector(ball_speed, (
-                simple_direct_pass.target_ball_pos - simple_direct_pass.start_ball_pos).th())
-        # print(type(ball_vel))
-        ball_pos: Vector2D = simple_direct_pass.start_ball_pos
-        travel_dist = 0
-        cycle = 0
-        # print(ball_pos)
-        # print(simple_direct_pass.target_ball_pos)
-        # print(pass_dist)
-        while travel_dist < pass_dist and ball_speed >= 0.1:
-            # dlog.add_circle(Level.PASS, Circle2D(ball_pos, 1.0))
-            cycle += 1
-            travel_dist += ball_speed
-            ball_speed *= SP.i().ball_decay()
-            ball_pos += ball_vel
-            ball_vel.set_length(ball_speed)
-            if self.can_opponent_cut_ball(wm, ball_pos, cycle):
-                return
-        self.candidates.append(simple_direct_pass)
+    def generate_direct_pass(self, wm: WorldModel, t):
+        sp = ServerParam.i()
+        MIN_RECEIVE_STEP = 3
+
+        MAX_DIRECT_PASS_DIST = 0.8 * inertia_final_distance(sp.ball_speed_max(), sp.ball_decay())
+        MAX_RECEIVE_BALL_SPEED = sp.ball_speed_max() * pow(sp.ball_decay(), MIN_RECEIVE_STEP)
+        receiver = wm.our_player(t)
+        MIN_DIRECT_PASS_DIST = receiver.player_type().kickable_area() * 2.2
+        if receiver.pos().x() > sp.pitch_half_length() - 1.5 or receiver.pos().x() < -sp.pitch_half_length() + 5.0 or receiver.pos().absY() > sp.pitch_half_width() - 1.5:
+            return
+        # TODO sp.ourTeamGoalPos()
+        if receiver.pos().x() < wm.ball().pos().x() + 1.0 and receiver.pos().dist2(Vector2D(-52.5,0)) < pow(18.0, 2):
+            return
+
+        ptype = receiver.player_type()
+        max_ball_speed = wm.self().kick_rate() * sp.max_power()
+        if wm.game_mode().type() == GameModeType.PlayOn:
+            max_ball_speed = sp.ball_speed_max()
+
+        # TODO SP.defaultRealSpeedMax()
+        min_ball_speed = 1.0
+
+        receive_point = ptype.inertiaFinalPoint(receiver.pos(), receiver.vel())
+        ball_move_dist = wm.ball().pos().dist(receive_point)
+
+        if ball_move_dist < MIN_DIRECT_PASS_DIST or MAX_DIRECT_PASS_DIST < ball_move_dist:
+            return
+
+        if wm.game_mode().type() in [GameModeType.GoalKick_Left, GameModeType.GoalKick_Right] \
+            and receive_point.x < sp.our_penalty_area_line_x() + 1.0 \
+                and receive_point.absY() < sp.penalty_area_half_width() + 1.0:
+            return
+
+        max_receive_ball_speed = min(MAX_RECEIVE_BALL_SPEED, ptype.kickable_area() + (sp.max_dash_power() * ptype.dash_power_rate() * ptype.effort_max()) * 1.8)
+        min_receive_ball_speed = ptype.real_speed_max()
+
+        ball_move_angle = (receive_point - wm.ball().pos()).th()
+
+        min_ball_step = sp.ball_move_step(sp.ball_speed_max(), ball_move_dist)
+        # TODO Penalty step
+        start_step = max(max(MIN_RECEIVE_STEP, min_ball_step), 0)
+        max_step = start_step + 2
+        self.create_pass(wm, receiver, receive_point,
+                          start_step, max_step, min_ball_speed,
+                          max_ball_speed, min_receive_ball_speed,
+                          max_receive_ball_speed, ball_move_dist,
+                          ball_move_angle, "D")
+
+    def generate_lead_pass(self, wm: WorldModel, t):
+        pass
+
+    def generate_through_pass(self, wm: WorldModel, t):
+        pass
+
+    def create_pass(self, wm: WorldModel, receiver, receive_point: Vector2D,
+                    min_step, max_step, min_first_ball_speed, max_first_ball_speed,
+                    min_receive_ball_speed, max_receive_ball_speed,
+                    ball_move_dist, ball_move_angle: AngleDeg, description):
+        sp = ServerParam.i()
+
+        for step in range(min_step, max_step + 1):
+            first_ball_speed = calc_first_term_geom_series(ball_move_dist, sp.ball_decay(), step)
+
+            if first_ball_speed < min_first_ball_speed:
+                break
+
+            if max_first_ball_speed < first_ball_speed:
+                continue
+
+            receive_ball_speed = first_ball_speed * pow(sp.ball_decay(), step)
+
+            if receive_ball_speed < min_receive_ball_speed:
+                break
+
+            if max_receive_ball_speed < receive_ball_speed:
+                continue
+
+            kick_count = predict_kick_count(wm, wm.self().unum(), first_ball_speed, ball_move_angle )
+
+            o_step = self.predict_opponents_reach_step(wm, wm.ball().pos(), first_ball_speed, ball_move_angle, receive_point, step + (kick_count - 1) + 5, description)
+
+            failed = False
+            if description == 'T':
+                if o_step <= step:
+                    failed = True
+            else:
+                if o_step <= step + (kick_count - 1):
+                   failed = True
+            if failed:
+                break
+
+            candidate = KickAction()
+            candidate.type = KickActionType.Pass
+            candidate.start_ball_pos = wm.ball().pos()
+            candidate.target_ball_pos = receive_point
+            candidate.target_unum = receiver
+            candidate.start_ball_speed = first_ball_speed
+            self.candidates.append(candidate)
+
+            find_another_pass = False
+            if not find_another_pass:
+                break
+
+            if o_step <= step + 3:
+                break
+
+            if min_step + 3 <= step:
+                break
+
+    def predict_opponents_reach_step(self, wm: WorldModel, first_ball_pos: Vector2D, first_ball_speed, ball_move_angle: AngleDeg, receive_point: Vector2D, max_cycle, description):
+        first_ball_vel = Vector2D.polar2vector(first_ball_speed, ball_move_angle)
+        min_step = 1000
+        for unum in range(12):
+            opp = wm.their_player(unum)
+            if opp.unum() == 0:
+                continue
+            step = self.predict_opponent_reach_step(wm, unum, first_ball_pos, first_ball_vel, ball_move_angle, receive_point, max_cycle, description)
+            if step < min_step:
+                min_step = step
+        return min_step
+
+    def predict_opponent_reach_step(self, wm: WorldModel, unum, first_ball_pos: Vector2D, first_ball_vel: Vector2D, ball_move_angle: AngleDeg, receive_point: Vector2D, max_cycle, description):
+        sp = ServerParam.i()
+        CONTROL_AREA_BUF = 0.15
+        opponent = wm.their_player(unum)
+        ptype = opponent.player_type()
+        min_cycle = estimate_min_reach_cycle(opponent.pos(), ptype.real_speed_max(), first_ball_pos, ball_move_angle)
+
+        if min_cycle < 0:
+            return 1000
+
+        for cycle in range(max( 1, min_cycle), max_cycle + 1):
+            ball_pos = inertia_n_step_point(first_ball_pos, first_ball_vel, cycle, sp.ball_decay())
+            control_area = ptype.kickable_area()
+
+            inertia_pos = ptype.inertiaPoint(opponent.pos(), opponent.vel(), cycle)
+            target_dist = inertia_pos.dist(ball_pos)
+
+            dash_dist = target_dist
+            # TODO calc Bonus
+            if dash_dist - control_area - CONTROL_AREA_BUF< 0.001:
+                return cycle
+
+            dash_dist -= control_area
+            if description != 'T':
+                if receive_point.x() < 25:
+                    dash_dist -= 0.5
+                else:
+                    dash_dist -= 0.2
+            if dash_dist > ptype.real_speed_max() * (cycle + min(opponent.pos_count(), 5)):
+                continue
+
+            n_dash = ptype.cycles_to_reach_distance(dash_dist)
+            if n_dash > cycle + opponent.pos_count():
+              continue
+
+            n_turn = 0
+            if opponent.body_count() > 1:
+                n_turn = predict_player_turn_cycle(ptype, opponent.body(), opponent.vel().r(), target_dist, (ball_pos - inertia_pos).th(), control_area, True)
+
+            n_step = n_turn + n_dash if n_turn == 0 else n_turn + n_dash + 1
+
+            bonus_step = 0
+            if opponent.is_tackling():
+                bonus_step = -5
+            if n_step - bonus_step <= cycle:
+                return cycle
+        return 1000
 
 
 class BhvDribbleGen(BhvKickGen):
@@ -165,7 +318,6 @@ class BhvDribbleGen(BhvKickGen):
 
             term = (1.0 - pow(sp.ball_decay(), 1 + n_turn + n_dash ) ) / (1.0 - sp.ball_decay())
             first_vel: Vector2D = (ball_trap_pos - wm.ball().pos()) / term
-            print(ball_trap_pos, wm.ball().pos(), term, first_vel)
             kick_accel: Vector2D = first_vel - wm.ball().vel()
             kick_power = kick_accel.r() / wm.self().kick_rate()
 
@@ -310,6 +462,6 @@ class BhvKick:
         target = best_action.target_ball_pos
         print(best_action)
         agent.debug_client().set_target(target)
-        agent.debug_client().add_message(best_action.type.value + 'to ' + best_action.target_ball_pos.__str__())
+        agent.debug_client().add_message(best_action.type.value + 'to ' + best_action.target_ball_pos.__str__() + ' ' + str(best_action.start_ball_speed))
         SmartKick(target, best_action.start_ball_speed, best_action.start_ball_speed - 1, 3).execute(agent)
 
