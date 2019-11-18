93dd08da316a2f7e5b246872bc783994b213d9c4
arefsa <arefsayareh@gmail.com>
2019-11-18 22:11:15 +0000

    starting tariner agent

---
+++ base/main_trainer.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from lib.player.basic_client import BasicClient
+import sys
+
+
+def main(team_name="Pyrus", goalie=False):
+    agent = TrainerAgent()
+    client = BasicClient()
+    agent.init(client)
+
+    client.run(agent)
+    # agent.run(team_name, goalie)
+
+
+if __name__ == "__main__":
+    goalie = False
+    if len(sys.argv) > 1 and sys.argv[1] == "g":
+        goalie = True
+    main(goalie=goalie)
--- lib/player/trainer_agent.py
+++ lib/player/trainer_agent.py
@@ -1,27 +1,20 @@
-import logging
-import socket
 import time
 
 from lib.action.kick_table import KickTable
-from base.decision import get_decision
 from lib.debug.logger import dlog
-from lib.math.angle_deg import AngleDeg
 from lib.player.soccer_agent import SoccerAgent
 from lib.player.world_model import WorldModel
-from lib.network.udp_socket import UDPSocket, IPAddress
 from lib.player_command.player_command import PlayerInitCommand, PlayerByeCommand
-from lib.player_command.player_command_body import PlayerTurnCommand, PlayerDashCommand, PlayerMoveCommand, \
-    PlayerKickCommand, PlayerTackleCommand
-from lib.player_command.player_command_support import PlayerDoneCommand, PlayerTurnNeckCommand
 from lib.player_command.player_command_sender import PlayerSendCommands
+from lib.player_command.player_command_support import PlayerDoneCommand
 from lib.rcsc.server_param import ServerParam
 
 
-class PlayerAgent(SoccerAgent):
+class TrainerAgent(SoccerAgent):
     class Impl:
         def __init__(self, agent):
             # TODO so many things....
-            self._agent: PlayerAgent = agent
+            self._agent: TrainerAgent = agent
             self._think_received = False
 
         def send_init_command(self):
@@ -47,30 +40,10 @@ class PlayerAgent(SoccerAgent):
 
     def __init__(self):
         super().__init__()
-        self._impl: PlayerAgent.Impl = PlayerAgent.Impl(self)
+        self._impl: TrainerAgent.Impl = TrainerAgent.Impl(self)
         self._world = WorldModel()
         self._full_world = WorldModel()
-        self._is_synch_mode = True
         self._last_body_command = []
-
-    def handle_start(self):
-        if self._client is None:
-            return False
-
-        # TODO check for config.host not empty
-
-        if not self._client.connect_to(IPAddress('localhost', 6000)):
-            print("ERROR failed to connect to server")
-            self._client.set_server_alive(False)
-            return False
-
-        self._impl.send_init_command()
-        return True
-
-    def handle_exit(self):
-        if self._client.is_server_alive():
-            self._impl.send_bye_command()
-        print(f"player( {self._world.self_unum()} ): finished")  # TODO : Not working
 
     def handle_message(self):
         self.run()
@@ -122,31 +95,11 @@ class PlayerAgent(SoccerAgent):
         elif message.find("think") is not -1:
             self._impl._think_received = True
 
-    def do_dash(self, power, angle=0):
-        self._last_body_command.append(PlayerDashCommand(power, float(angle)))
-        return True
-
-    def do_turn(self, angle):
-        self._last_body_command.append(PlayerTurnCommand(float(angle)))
-        return True
-
-    def do_move(self, x, y):
-        self._last_body_command.append(PlayerMoveCommand(x, y))
-        return True
-
-    def do_kick(self, power: float, rel_dir: AngleDeg):
-        self._last_body_command.append(PlayerKickCommand(power, rel_dir))
-        return True
-
-    def do_tackle(self, power_or_dir: float, foul: bool):  # TODO : tons of work
-        if self.world().self().is_frozen():
-            return False
-        self._last_body_command.append(PlayerTackleCommand(power_or_dir, foul))
-        return True
-
-    def do_turn_neck(self, moment: AngleDeg) -> bool:
-        self._last_body_command.append(PlayerTurnNeckCommand(moment))
-        return True
+    def init_dlog(self, message):
+        message = message.split(" ")
+        unum = int(message[2])
+        side = message[1]
+        dlog.setup_logger(f"dlog{side}{unum}", f"/tmp/{self.world().team_name()}-{unum}.log", logging.DEBUG)
 
     def world(self) -> WorldModel:
         return self._full_world
@@ -158,7 +111,7 @@ class PlayerAgent(SoccerAgent):
         if (self.world().self_unum() is None
                 or self.world().self().unum() != self.world().self_unum()):
             return
-        get_decision(self)
+        get_decision(self) # TODO DECISION?!?!?!?!
         commands = self._last_body_command
         # if self.world().our_side() == SideID.RIGHT:
         # PlayerCommandReverser.reverse(commands) # unused :\ # its useful :) # nope not useful at all :(
@@ -168,8 +121,3 @@ class PlayerAgent(SoccerAgent):
         dlog.flush()
         self._last_body_command = []
 
-    def init_dlog(self, message):
-        message = message.split(" ")
-        unum = int(message[2])
-        side = message[1]
-        dlog.setup_logger(f"dlog{side}{unum}", f"/tmp/{self.world().team_name()}-{unum}.log", logging.DEBUG)
