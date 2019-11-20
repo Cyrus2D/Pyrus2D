9a2b3688b294949c34047168d98f384bd24a096b
arefsa <arefsayareh@gmail.com>
2019-11-20 19:00:40 +0000

    add commands

--- lib/player_command/trainer_command.py
+++ lib/player_command/trainer_command.py
@@ -1,5 +1,8 @@
 from enum import Enum, unique, auto
 
+from lib.math.angle_deg import AngleDeg
+from lib.math.vector_2d import Vector2D
+
 
 class TrainerCommand:
     @unique
@@ -54,3 +57,59 @@ class TrainerSendCommands(TrainerCommand):
             commands_msg += command.str()  # TODO FIX THIS
         commands_msg = commands_msg
         return commands_msg
+
+
+class TrainerMoveBallCommand(TrainerCommand):
+    def __init__(self, pos: Vector2D, vel: Vector2D = None):
+        super().__init__()
+        self._pos = pos
+        self._vel = vel
+
+    def type(self):
+        return TrainerCommand.Type.MOVE
+
+    def str(self):
+        if self._vel is None:
+            return f"(move (ball) {self._pos.x()} {self._pos.y()})"
+        return f"(move (ball) {self._pos.x()} {self._pos.y()}" \
+               f" 0 {self._vel.x()} {self._vel.y()})"
+
+
+class TrainerMovePlayerCommand(TrainerCommand):
+    def __init__(self,
+                 teamname: str,
+                 unum: int,
+                 pos: Vector2D,
+                 angle: float = None,
+                 vel: Vector2D = None):
+        super().__init__()
+        self._teamname = teamname
+        self._unum = unum
+        self._pos = pos
+        self._angle = angle
+        self._vel = vel
+
+    def type(self):
+        return TrainerCommand.Type.MOVE
+
+    def str(self):
+        if not self.check():
+            return ""
+
+        if self._angle is None:
+            return f"(move (player {self._teamname} {self._unum}) " \
+                   f"{self._pos.x()} {self._pos.y()})"
+        else:
+            if self._vel is None:
+                return f"(move (player {self._teamname} {self._unum})" \
+                       f" {self._pos.x()} {self._pos.y()} {self._angle})"
+            else:
+                return f"(move (player {self._teamname} {self._unum}) " \
+                       f"{self._pos.x()} {self._pos.y()} {self._angle}" \
+                       f" {self._vel.x()} {self._vel.y()})"
+
+    def check(self):
+        if not 0 < self._unum < 12:
+            print("Illegal uniform number")
+            return False
+        return True
