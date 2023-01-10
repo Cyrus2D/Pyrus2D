062d0f03dcadc9dd65a4ca77d377f11b8fb104d5
ArefSa78 <arefsayareh@gmail.com>
2023-01-10 18:49:58 -0330

    add pre-processor

--- base/sample_player.py
+++ base/sample_player.py
@@ -1,10 +1,39 @@
 from base.decision import get_decision
+from base.view_tactical import ViewTactical
+from lib.action.neck_turn_to_ball_or_scan import NeckTurnToBallOrScan
 from lib.player.player_agent import PlayerAgent
+from lib.rcsc.types import GameModeType
+
 
 class SamplePlayer(PlayerAgent):
     def __init__(self):
         super().__init__()
     
     def action_impl(self):
+        if self.do_preprocess():
+            return
+
         get_decision(self)
-    
+
+    def do_preprocess(self):
+        wm = self.world()
+
+        if wm.self().is_frozen():
+            self.set_view_action(ViewTactical())
+            self.set_neck_action(NeckTurnToBallOrScan())
+            return True
+
+        if not wm.self().pos_valid():
+            self.set_view_action(ViewTactical())
+            ScanField().execute(self) # TODO IMP FUNC
+            return True
+
+        count_thr = 10 if wm.self().goalie() else 5
+        if wm.ball().pos_count() > count_thr or ( wm.game_mode().type() is not GameModeType.PlayOn and wm.ball().seen_pos_count() > count_thr + 10):
+            self.set_view_action(ViewTactical())
+            NeckBodyToBall().execute(agent) # TODO IMP FUNC
+            return True
+
+        self.set_view_action(ViewTactical())
+
+        return False
