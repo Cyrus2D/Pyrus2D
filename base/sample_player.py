8e487a8cd7390dc74cad2ed5ee094d980945ea78
ArefSa78 <arefsayareh@gmail.com>
2023-01-10 23:48:28 -0330

    add pre process to sample player

--- base/sample_player.py
+++ base/sample_player.py
@@ -1,6 +1,8 @@
 from base.decision import get_decision
 from base.view_tactical import ViewTactical
+from lib.action.neck_body_to_ball import NeckBodyToBall
 from lib.action.neck_turn_to_ball_or_scan import NeckTurnToBallOrScan
+from lib.action.scan_field import ScanField
 from lib.player.player_agent import PlayerAgent
 from lib.rcsc.types import GameModeType
 
@@ -25,13 +27,13 @@ class SamplePlayer(PlayerAgent):
 
         if not wm.self().pos_valid():
             self.set_view_action(ViewTactical())
-            ScanField().execute(self) # TODO IMP FUNC
+            ScanField().execute(self)
             return True
 
         count_thr = 10 if wm.self().goalie() else 5
         if wm.ball().pos_count() > count_thr or ( wm.game_mode().type() is not GameModeType.PlayOn and wm.ball().seen_pos_count() > count_thr + 10):
             self.set_view_action(ViewTactical())
-            NeckBodyToBall().execute(agent) # TODO IMP FUNC
+            NeckBodyToBall().execute(self)
             return True
 
         self.set_view_action(ViewTactical())
