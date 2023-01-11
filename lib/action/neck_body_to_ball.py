75db07e25ef5f8bd064f053c65a1cc0525ff20ed
ArefSa78 <arefsayareh@gmail.com>
2023-01-10 23:46:46 -0330

    add nec body to ball action

---
+++ lib/action/neck_body_to_ball.py
@@ -0,0 +1,23 @@
+from pyrusgeom.angle_deg import AngleDeg
+
+from lib.action.neck_body_to_point import NeckBodyToPoint
+from lib.action.scan_field import ScanField
+from lib.player.soccer_action import NeckAction
+
+from typing import TYPE_CHECKING, Union
+
+if TYPE_CHECKING:
+    from lib.player.player_agent import PlayerAgent
+
+
+class NeckBodyToBall(NeckAction):
+    def __init__(self, angle_buf: Union[AngleDeg,float] = 5.):
+        self._angle_buf = float(angle_buf)
+
+    def execute(self, agent: 'PlayerAgent'):
+        wm = agent.world()
+        if wm.ball().pos_valid():
+            ball_next = wm.ball().pos() + wm.ball().vel()
+
+            return NeckBodyToPoint(ball_next, self._angle_buf).execute(agent)
+        return ScanField().execute(agent)
