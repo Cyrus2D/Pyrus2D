31a3f403846e949f5e730bac8717426c4475f54c
ArefSa78 <arefsayareh@gmail.com>
2023-01-18 18:48:21 -0330

    now players do preprocess when they hear a pass message

--- base/sample_player.py
+++ base/sample_player.py
@@ -1,10 +1,16 @@
 from base.decision import get_decision
 from base.view_tactical import ViewTactical
+from lib.action.go_to_point import GoToPoint
+from lib.action.intercept import Intercept
 from lib.action.neck_body_to_ball import NeckBodyToBall
+from lib.action.neck_turn_to_ball import NeckTurnToBall
 from lib.action.neck_turn_to_ball_or_scan import NeckTurnToBallOrScan
 from lib.action.scan_field import ScanField
 from lib.debug.debug_print import debug_print
+from lib.debug.level import Level
+from lib.debug.logger import dlog
 from lib.player.player_agent import PlayerAgent
+from lib.rcsc.server_param import ServerParam
 from lib.rcsc.types import GameModeType
 
 
@@ -31,7 +37,6 @@ class SamplePlayer(PlayerAgent):
         get_decision(self)
         debug_print(f"{'#'*20}END{'#'*20}")
 
-
     def do_preprocess(self):
         wm = self.world()
 
@@ -54,4 +59,64 @@ class SamplePlayer(PlayerAgent):
 
         self.set_view_action(ViewTactical())
 
+        if self.do_heard_pass_receive():
+            return True
+
         return False
+
+    def do_heard_pass_receive(self):
+        wm = self.world()
+
+        if wm.messenger_memory().pass_time() != wm.time() \
+            or len(wm.messenger_memory().pass_()) == 0 \
+            or wm.messenger_memory().pass_()[0]._receiver != wm.self().unum():
+
+            return False
+
+        self_min = wm.intercept_table().self_reach_cycle()
+        intercept_pos = wm.ball().inertia_point(self_min)
+        heard_pos = wm.messenger_memory().pass_()[0]._pos
+
+        dlog.add_text(Level.TEAM, f"(sample palyer do heard pass) heard_pos={heard_pos}, intercept_pos={intercept_pos}")
+
+        if not wm.kickable_teammate() \
+            and wm.ball().pos_count() <= 1 \
+            and wm.ball().vel_count() <= 1 \
+            and self_min < 20:
+            dlog.add_text(Level.TEAM, f"(sample palyer do heard pass) intercepting!, self_min={self_min}")
+            self.debug_client().add_message("Comm:Receive:Intercept")
+            Intercept().execute(self)
+            self.set_neck_action(NeckTurnToBall())
+        else:
+            dlog.add_text(Level.TEAM, f"(sample palyer do heard pass) go to point!, cycle={self_min}")
+            self.debug_client().set_target(heard_pos)
+            self.debug_client().add_message("Comm:Receive:GoTo")
+
+            GoToPoint(heard_pos, 0.5, ServerParam.i().max_dash_power()).execute(self)
+            self.set_neck_action(NeckTurnToBall())
+
+        # TODO INTENTION?!?
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
