2edd40cac1824b88217f6b810c9ff025ede189c5
ArefSa78 <arefsayareh@gmail.com>
2023-01-18 18:45:01 -0330

    add pass messenger to bhv kick

--- base/bhv_kick.py
+++ base/bhv_kick.py
@@ -2,22 +2,26 @@ from lib.action.hold_ball import HoldBall
 from lib.action.neck_scan_players import NeckScanPlayers
 from lib.action.smart_kick import SmartKick
 from typing import List
-from base.generator_action import KickAction, ShootAction
+from base.generator_action import KickAction, ShootAction, KickActionType
 from base.generator_dribble import BhvDribbleGen
 from base.generator_pass import BhvPassGen
 from base.generator_shoot import BhvShhotGen
 from lib.debug.debug_print import debug_print
 
 from typing import TYPE_CHECKING
+
+from lib.messenger.pass_messenger import PassMessenger
+
 if TYPE_CHECKING:
     from lib.player.world_model import WorldModel
+    from lib.player.player_agent import PlayerAgent
 
 
 class BhvKick:
     def __init__(self):
         pass
 
-    def execute(self, agent):
+    def execute(self, agent: 'PlayerAgent'):
         wm: 'WorldModel' = agent.world()
         shoot_candidate: ShootAction = BhvShhotGen().generator(wm)
         if shoot_candidate:
@@ -44,5 +48,12 @@ class BhvKick:
             agent.debug_client().set_target(target)
             agent.debug_client().add_message(best_action.type.value + 'to ' + best_action.target_ball_pos.__str__() + ' ' + str(best_action.start_ball_speed))
             SmartKick(target, best_action.start_ball_speed, best_action.start_ball_speed - 1, 3).execute(agent)
+
+            if best_action.type is KickActionType.Pass:
+                agent.add_say_message(PassMessenger(best_action.target_unum,
+                                                    best_action.target_ball_pos,
+                                                    agent.effector().queued_next_ball_pos(),
+                                                    agent.effector().queued_next_ball_vel()))
+
             agent.set_neck_action(NeckScanPlayers())
             return True
