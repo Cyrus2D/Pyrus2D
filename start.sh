5918eb2352c32d75476c58698da61c68ff761e6e
nader <nader.zare88@gmail.com>
2023-03-08 16:02:34 -0400

    update start.sh

--- start.sh
+++ start.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 
-# export PYTHONPATH="${PYTHONPATH}:`pwd`"
+export PYTHONPATH="${PYTHONPATH}:`pwd`"
 
-#./base/main_player.py g &
-
+python base/main_player.py 1 g > "player-1-log.txt" 2> "player-1-error.txt" &
+sleep 1
 i=1
 while [ $i -le 11 ] ; do
-    python main.py > "player-$i-log.txt" 2> "player-$i-error.txt" &
+    python base/main_player.py $i > "player-$i-log.txt" 2> "player-$i-error.txt" &
     sleep 0.01
 
   i=`expr $i + 1`
 done
 
-sleep 5
+sleep 2
 python coach_main.py > "coach-log.txt" 2> "coach-error.txt"
