33d31a7f40bf38fb9c71960cbb25cc7691b763bb
nader <nader.zare88@gmail.com>
2023-03-13 23:12:22 -0300

    FFFIIIXXX VERY BAD BUGGGG

--- lib/parser/message_params_parser_see.py
+++ lib/parser/message_params_parser_see.py
@@ -2,12 +2,12 @@ class MessageParamsParserSee:
     def __init__(self) -> None:
         pass
 
-    def parse(self, string) -> dict:
-        dic = {}
+    def parse(self, string) -> list:
+        res = []
         objects_start_index = string.find("((")
 
         if objects_start_index == -1:
-            return None
+            return []
 
         objects_string = string[objects_start_index: -1]
         objects_list_string = objects_string[1:-1].split(") (")
@@ -15,6 +15,5 @@ class MessageParamsParserSee:
         for object_string in objects_list_string:
             key_end_index = object_string.find(")")
             key = object_string[1:key_end_index]
-            dic[key] = object_string[key_end_index+1:].strip(" ")
-        
-        return dic
+            res.append((key, object_string[key_end_index+1:].strip(" ")))
+        return res
