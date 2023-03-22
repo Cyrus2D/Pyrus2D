a3d93bae57f7aeaca97d8331eff5375932931546
Aref <arefsayareh@gmail.com>
2023-03-22 15:48:34 -0230

    add converter class

--- lib/messenger/converters.py
+++ lib/messenger/converters.py
@@ -1,36 +1,111 @@
+from math import ceil
 from typing import Union
-
 
 chars = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ ().+-*/?<>_0123456789"
 
-def convert_to_words(val: int, size: int) -> str:
+
+class MessengerConverter:
+    def __init__(self, min_max_sizes: list[tuple[float, float, int]]):
+        self._min_max_sizes = min_max_sizes
+
+    def convert_to_word(self, values):
+        s = 0
+        for i, (min_v, max_v, size) in enumerate(self._min_max_sizes):
+            v = values[i]
+            v -= min_v
+            v /= max_v - min_v
+            v *= size
+
+            s *= size
+            s += int(v)
+
         n_chars = len(chars)
         words = []
-        for _ in range(size - 1):
-            words.append(val%n_chars)
-            val = val // n_chars
-        
-        words.append(val)
+        while s != 0:
+            words.append(s % n_chars)
+            s = s // n_chars
+
+        words.append(s)
         msg = ''
         for word in words:
             msg += chars[word]
-        
+
         return msg
 
-def convert_to_int(msg: str)  -> Union[int, None]:
+    def convert_to_values(self, msg):
+        if msg == '':
+            return None
+
+        msg = msg[::-1]
+
+        n_chars = len(chars)
+        digit = len(msg) - 1
+
+        val: int = 0
+
+        for c in msg:
+            i = chars.find(c)
+            val += i * int(n_chars ** digit)
+            digit -= 1
+
+        values = []
+        for i, (min_v, max_v, size) in enumerate(self._min_max_sizes[::-1]):
+            v = val % size
+            v = v / size * (max_v - min_v)
+            v = v + min_v
+
+            values.append(v)
+            val = int(val / size)
+        return values[::-1]
+
+
+def convert_to_bits(values_min_max_sizes: list[tuple[float, float, float, int]]):
+    s = 0
+    for i, (v, min_v, max_v, size) in enumerate(values_min_max_sizes):
+        v -= min_v
+        v /= max_v - min_v
+        v *= size
+
+        s += int(v)
+
+        print(f'{int(v)}, {s}', end=', ')
+        if i != len(values_min_max_sizes) - 1:
+            s *= values_min_max_sizes[i + 1][-1]
+        print(s)
+    return s
+
+
+#  bYl)0L
+
+def convert_to_words(val: int, size: int) -> str:
+    n_chars = len(chars)
+    words = []
+    for _ in range(size - 1):
+        words.append(val % n_chars)
+        val = val // n_chars
+
+    words.append(val)
+    msg = ''
+    for word in words:
+        msg += chars[word]
+
+    return msg
+
+
+def convert_to_int(msg: str) -> Union[int, None]:
     if msg == '':
         return None
-    
+
     msg = msg[::-1]
-    
+
     n_chars = len(chars)
     digit = len(msg) - 1
-    
+
     val: int = 0
-    
+
     for c in msg:
         i = chars.find(c)
-        val += i * int(n_chars**digit)
+        val += i * int(n_chars ** digit)
         digit -= 1
-    
+
     return val
