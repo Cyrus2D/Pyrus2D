9c2e488c91e428048f28fe06ddaac354cea9deb9
Aref <arefsayareh@gmail.com>
2023-03-28 13:02:47 -0230

    add goalie to memory messenger

--- lib/messenger/messenger_memory.py
+++ lib/messenger/messenger_memory.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+from pyrusgeom.angle_deg import AngleDeg
 from pyrusgeom.vector_2d import Vector2D
 from lib.rcsc.game_time import GameTime
 
@@ -6,8 +9,8 @@ class MessengerMemory:
     class Object:
         def __init__(self, sender = 0, pos: Vector2D = Vector2D(), vel: Vector2D = None) -> None:
             self.sender_: int = sender
-            self.pos_: Vector2D = pos
-            self.vel_: Vector2D = vel
+            self.pos_: Vector2D = pos.copy()
+            self.vel_: Vector2D = vel.copy()
 
     class Player(Object):
         def __init__(self,
@@ -21,10 +24,16 @@ class MessengerMemory:
             self.unum_: int = unum
             self.body_: float = body
             self.stamina_: float = stamina
-    
+
     class Ball(Object):
         def __init__(self, sender=0, pos: Vector2D = Vector2D(), vel: Vector2D = None) -> None:
             super().__init__(sender, pos, vel)
+
+    class Goalie:
+        def __init__(self, sender = 0, pos: Vector2D = Vector2D(), body: AngleDeg = None):
+            self.sender_ = sender
+            self.pos_ = pos.copy()
+            self.body_ = body.copy()
 
     class Pass:
         def __init__(self, sender: int, receiver: int, pos: Vector2D):
@@ -37,12 +46,15 @@ class MessengerMemory:
 
         self._players: list[MessengerMemory.Player] = []
         self._player_time: GameTime = GameTime()
-        
+
         self._balls: list[MessengerMemory.Ball] = []
         self._ball_time: GameTime = GameTime()
 
         self._pass: list[MessengerMemory.Pass] = []
         self._pass_time: GameTime = GameTime()
+
+        self._goalie: list[MessengerMemory.Goalie] = []
+        self._goalie_time: GameTime = GameTime()
 
         self._player_record: list[tuple[GameTime, MessengerMemory.Player]] = []
 
@@ -53,8 +65,8 @@ class MessengerMemory:
         self._balls.append(MessengerMemory.Ball(sender, pos, vel))
         self._ball_time = current_time.copy()
         self._time = current_time.copy()
-    
-    def add_player(self, sender: int, unum: int, pos: Vector2D, current_time: GameTime):
+
+    def add_player(self, sender: int, unum: int, pos: Vector2D, current_time: GameTime, body=None):
         if self._player_time != current_time:
             self._players.clear()
 
@@ -75,19 +87,31 @@ class MessengerMemory:
         self._pass_time = current.copy()
 
         self._time = current.copy()
-    
+
+    def add_opponent_goalie(self, sender:int, pos: Vector2D, body: Union[AngleDeg, float], current_time: GameTime):
+        if self._goalie_time != current_time:
+            self._goalie.clear()
+
+        self._goalie.append(MessengerMemory.Goalie(sender, pos, AngleDeg(body)))
+        self._goalie_time = current_time.copy()
+
+        self._time = current_time.copy()
+
+    def goalie_time(self):
+        return self._goalie_time
+
     def time(self):
         return self._time
-        
+
     def players(self):
         return self._players
-        
+
     def player_time(self):
         return self._player_time
-        
+
     def balls(self):
         return self._balls
-        
+
     def ball_time(self):
         return self._ball_time
 
@@ -99,5 +123,4 @@ class MessengerMemory:
 
     def player_record(self):
         return self._player_record
-    
-    
+
